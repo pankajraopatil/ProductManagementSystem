4791db60ff58cec77efe67e9ea3ae06e489784ff
Pankaj <Pankajraopatil@gmail.com>
2021-11-09 12:08:54 +0530

    added by pankaj

---
+++ Products.Entity/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Products.Entity
+{
+    class Sample
+    {
+        public int MyProperty1 { get; set; }
+        public string MyProperty2 { get; set; }
+    }
+}
